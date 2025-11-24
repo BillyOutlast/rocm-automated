65dec81e73b7d84b505a5232db2d6c3bd3276f4e
John Doe <johndoe@example.com>
2025-11-23 22:46:01 -0500

    suggested nodes and adding git

---
+++ comfyui-setup.sh
@@ -0,0 +1,22 @@
+cd User-Directories/ComfyUI/custom_nodes
+
+# Manager
+git clone https://github.com/ltdrdata/ComfyUI-Manager comfyui-manager
+
+# Crystools (AMD branch)
+git clone -b AMD https://github.com/crystian/ComfyUI-Crystools.git
+
+# MIGraphX
+git clone https://github.com/pnikolic-amd/ComfyUI_MIGraphX.git
+
+# Unsafe Torch
+git clone https://github.com/ltdrdata/comfyui-unsafe-torch
+
+# Impact Pack
+git clone https://github.com/ltdrdata/ComfyUI-Impact-Pack comfyui-impact-pack
+
+# Impact Subpack
+git clone https://github.com/ltdrdata/ComfyUI-Impact-Subpack
+
+# WaveSpeed
+git clone https://github.com/chengzeyi/Comfy-WaveSpeed.git
